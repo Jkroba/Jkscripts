3e0f6068e2c3e8c18224d2d56c5c1f741b46856e
Jkroba <71403495+Jkroba@users.noreply.github.com>
2020-09-16 15:25:12 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# todos os creditos para daniel vitor caetano albieri
-sudo apt-get update
-sudo apt-get install python3
-pip3 install subprocess.run
-echo "instalacao completa"
-echo "para iniciar o script use (python3 lazyscript.py) 
